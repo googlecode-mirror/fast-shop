08ef9b5980daeb784918a412860249f55ef54f2e
thachvis91 <thachvis91@gmail.com>
2011-10-04 22:01:50 +0000

    đang đợi db

---
+++ SourceCode/regProccess.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+
+$txtUser = $_POST['txtUser'];
+$txtPass = $_POST['txtPass'];
+$txtRePass = $_POST['txtRePass'];
+$txtEmail = $_POST['txtEmail'];
+$txtName = $_POST['txtName'];
+$txtAdd = $_POST['txtAdd'];
+$txtPhone = $_POST['txtPhone'];
+?>
+</body>
+</html>
