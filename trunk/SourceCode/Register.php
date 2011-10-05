c6c6f160418fbf1b47e151b56951d519476679dd
thachvis91@gmail.com <thachvis91@gmail.com@3c4ff42c-72a5-1d63-0403-34e5de3b5372>
2011-10-05 12:28:24 +0000

    đâsd

--- trunk/SourceCode/Register.php
+++ trunk/SourceCode/Register.php
@@ -3,14 +3,14 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Untitled Document</title>
-<script language="JavaScript" src="C:\xampp\htdocs\code\js\validate.js"></script>
+<script language="javascript" src="C:\xampp\htdocs\code\js\validate.js"></script>
 <script language="javascript">
 function validatetxtUser(){
 	var x=document.forms['frmRegister']['txtUser'].value;
 	var temp1=new RegExp("[A-Z]");
 	var temp2=new RegExp("[a-z]");
 	if (x==null || x=="" || x.length<4 || x.length>12){
-		document.write("<p>Tên đăng nhập phải từ 4-12 ký tự</p>");
+		document.write("Tên đăng nhập phải từ 4-12 ký tự");
 		return false;
 	}
 	else if (temp1.test(x.substring(0,1))==false && temp2.test(x.substring(0,1))==false){
@@ -47,7 +47,7 @@ function validatetxtName(){
 function validatetxtAdd(){
 	var x=document.forms['frmRegister']['txtAdd'].value;
 	if (x==null || x==""){
-		document.write("<p>Địa chỉ không được để trống</p>");
+		document.write("Địa chỉ không được để trống");
 		return false;
 	}
 }
@@ -76,49 +76,100 @@ function validatetxtEmail(){
 <form action="regProccess.php" method="post" name="frmRegister" target="_parent" id="frmRegister">
   <table width="50%" border="0" align="center">
     <tr>
-      <td colspan="2"><div align="center"><strong>ĐĂNG KÝ THÔNG TIN THÀNH VIÊN</strong></div></td>
+      <td colspan="3"><div align="center"><strong>ĐĂNG KÝ THÔNG TIN THÀNH VIÊN</strong></div></td>
+    </tr>
+    <tr>
+      <td width="44%"><div align="right"></div></td>
+      <td width="27%">&nbsp;</td>
+      <td width="29%">&nbsp;</td>
     </tr>
     <tr>
       <td><div align="right">Tên đăng nhập</div></td>
       <td>
       <input type="text" name="txtUser" id="txtUser" onblur="validatetxtUser()" /></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td><div align="right"></div></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
     </tr>
     <tr>
       <td><div align="right">Mật khẩu</div></td>
       <td>
           <input type="password" name="txtPass" id="txtPass" onblur="validatetxtPass()"/></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td><div align="right"></div></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
     </tr>
     <tr>
       <td><div align="right">Xác nhận mật khẩu </div></td>
       <td>
           <input type="password" name="txtRePass" id="txtRePass" onblur="validatetxtRePass()"/></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
     </tr>
     <tr>
       <td><div align="right">Email</div></td>
       <td>
           <input type="text" name="txtEmail" id="txtEmail" onblur="validatetxtEmail()"/></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td><div align="right"></div></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
     </tr>
     <tr>
       <td><div align="right">Họ và tên </div></td>
       <td>
           <input type="text" name="txtName" id="txtName" onblur="validatetxtName()"/></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td><div align="right"></div></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
     </tr>
     <tr>
       <td><div align="right">Địa chỉ thường trú </div></td>
       <td>
           <input type="text" name="txtAdd" id="txtAdd" onblur="validatetxtAdd()"/></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td><div align="right"></div></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
     </tr>
     <tr>
       <td><div align="right">Điện thoại liên hệ</div></td>
-      <td>
-          <input type="text" name="txtPhone" id="txtPhone" onblur="validatetxtPhone()"/></td>
+      <td><input type="text" name="txtPhone" id="txtPhone" onblur="validatetxtPhone()"/></td>
+      <td>&nbsp;</td>
     </tr>
     <tr>
-      <td colspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td colspan="3">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <input type="checkbox" name="cbAgree" value="checkbox" />
     <label for="txtPass"> Tôi đã đọc và chấp nhận các điều khoản của Website </label></td></tr>
     <tr>
-      <td colspan="2"><div align="center">
+      <td><div align="right"></div></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td colspan="3"><div align="center">
         <input type="submit" name="btnSubmit" id="btnSubmit" value="Đăng ký" />
         <input type="reset" name="btnReset" id="btnReset" value="Làm lại" />
       </div></td>
